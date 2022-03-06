d29cb21b9d84849b1f398f1d032f7d06828f3250
Tural Novruzov <TuraliNovruzov@outlook.com>
2022-03-06 20:06:15 +0400

    Add project files.

---
+++ cs_UdpClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace cs_UdpClient
+{
+    class Program
+    {
+        static void Main()
+        {
+            var client = new UdpClient();
+
+            var connectEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 45678);
+
+            while (true)
+            {
+                var str = Console.ReadLine();
+                var bytes = Encoding.Default.GetBytes(str);
+                client.Send(bytes, bytes.Length, connectEP);
+            }
+        }
+    }
+}
---
+++ cs_UdpServer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace cs_UdpServer
+{
+    class Program
+    {
+        static void Main()
+        {
+            var client = new UdpClient(45678);
+
+            var remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+            while (true)
+            {
+                var bytes = client.Receive(ref remoteEP);
+                var str = Encoding.Default.GetString(bytes);
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
